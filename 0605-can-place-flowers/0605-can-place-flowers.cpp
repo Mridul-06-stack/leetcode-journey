48dcc3f5abf76fb24cd117c8cd28653d67bf9d6c
Mridul-06-stack <24bec066@nith.ac.in>
2026-07-16 16:56:00 +0530

    Time: 0 ms (100%), Space: 24.1 MB (44.95%) - LeetHub

---
+++ 0605-can-place-flowers/0605-can-place-flowers.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    bool canPlaceFlowers(vector<int>& flowerbed, int n) {
+        int count=0;
+        int i=0;
+        int size=flowerbed.size();
+        while(i<size&&count<n){
+           if(flowerbed[i]==0){
+            if((i==0||flowerbed[i-1]==0)&&(i==size-1||flowerbed[i+1]==0)){
+                flowerbed[i]=1;
+                count++;
+              
+            }
+
+
+
+           }  
+           i++;
+        }
+   
+   
+   
+   return count==n? 1:0;
+    }
+};
