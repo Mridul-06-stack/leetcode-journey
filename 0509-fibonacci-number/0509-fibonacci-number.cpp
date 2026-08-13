1917334b9a17517d26f74aa0622b10fde548ad11
Mridul-06-stack <24bec066@nith.ac.in>
2026-08-14 00:49:10 +0530

    Time: 3 ms (64.79%), Space: 8.2 MB (11.4%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int helper(int n,vector<int> &dp){
+    if(n<=1) return n;
+    if(dp[n]==-1){
+        dp[n]=helper(n-1,dp)+helper(n-2,dp);
+    }
+    return dp[n];
+
+
+
+
+
+    }
+    int fib(int n) {
+         vector<int> dp(n+1,-1);
+         return helper(n,dp);
+    }
+};
